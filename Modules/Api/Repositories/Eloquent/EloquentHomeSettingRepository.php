e3f029bc439364a98793a93f29a75931567542f4
Linh Pham <phamlinh0208@gmail.com>
2021-04-14 16:50:56 +0700

    fix api home

--- Modules/Api/Repositories/Eloquent/EloquentHomeSettingRepository.php
+++ Modules/Api/Repositories/Eloquent/EloquentHomeSettingRepository.php
@@ -11,31 +11,36 @@ use Modules\Mon\Entities\HomeSetting;
 use Modules\Mon\Entities\Pcategory;
 use Modules\Mon\Entities\Product;
 
-class EloquentHomeSettingRepository implements HomeSettingRepository
-{
+class EloquentHomeSettingRepository implements HomeSettingRepository {
 
 	public function get() {
 		$homesettings = HomeSetting::query()->orderBy('order_')->get();
 		$data = [];
 		foreach ($homesettings as $homesetting) {
-			$listId = explode(',',$homesetting->content);
+			$listId = explode(',', $homesetting->content);
+			$item = [
+				'type' => $homesetting->type,
+				'title' => $homesetting->title
+			];
 			switch ($homesetting->type) {
 				case HomeSetting::TYPE_BANNER:
 					$banners = Banners::query()->whereIn('id', $listId)->get();
-					$data[] = HomeBannerTransformer::collection($banners);
+
+					$item['data'] = HomeBannerTransformer::collection($banners);
 					break;
 				case HomeSetting::TYPE_BEST_SELL:
 				case HomeSetting::TYPE_BUY_NOW:
 					$products = Product::query()->whereIn('id', $listId)->get();
-					$data[] = HomeProductTransformer::collection($products);
+					$item['data'] = HomeProductTransformer::collection($products);
 					break;
 
 				case HomeSetting::TYPE_CATEGORY:
 					$categories = Pcategory::query()->whereIn('id', $listId)->get();
-					$data[] = HomePCategoryTransformer::collection($categories);
+					$item['data'] = HomePCategoryTransformer::collection($categories);
 					break;
 
 			}
+			$data[] = $item;
 		}
 		return $data;
 	}
