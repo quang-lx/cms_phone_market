94b8b7af3d1db8741acaa62e846565f46e0ed944
Linh Pham <phamlinh0208@gmail.com>
2021-04-16 00:07:50 +0700

    api search/suggest

---
+++ Modules/Api/Http/Controllers/SearchController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Modules\Api\Http\Controllers;
+
+use App\Events\NeedCreateUserSmsToken;
+use App\Rules\PhoneNumber;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Validator;
+use Modules\Api\Entities\ErrorCode;
+use Modules\Api\Repositories\SearchRepository;
+use Modules\Api\Transformers\UserTransformer;
+use Modules\Mon\Auth\Contracts\Authentication;
+use Modules\Mon\Entities\SmsToken;
+use Modules\Mon\Entities\User;
+use Modules\Mon\Http\Controllers\ApiController;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class SearchController extends ApiController
+{
+
+    /** @var SearchRepository */
+    public $searchRepo;
+
+    public function __construct(Authentication $auth, SearchRepository $searchRepo)
+    {
+        parent::__construct($auth);
+
+        $this->searchRepo = $searchRepo;
+    }
+    public function listSuggestion(Request $request)
+    {
+        $data = $this->searchRepo->listSuggestion($request);
+        return $this->respond($data, ErrorCode::SUCCESS_MSG, ErrorCode::SUCCESS);
+    }
+
+}
---
+++ Modules/Api/Repositories/SearchRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Modules\Api\Repositories;
+
+
+use Illuminate\Http\Request;
+
+interface SearchRepository
+{
+    public function listSuggestion(Request $request);
+}
