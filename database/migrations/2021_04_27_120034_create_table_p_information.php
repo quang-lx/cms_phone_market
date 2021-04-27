9f58aa7a175cdad65934f9caffccd52591f944e6
Linh Pham <phamlinh0208@gmail.com>
2021-04-27 12:03:38 +0700

    product info

---
+++ database/migrations/2021_04_27_120034_create_table_p_information.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTablePInformation extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('p_information', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+	        $table->unsignedBigInteger('shop_id')->nullable();
+	        $table->unsignedBigInteger('company_id')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+	        $table->index('shop_id');
+	        $table->index('shop_id');
+
+        });
+	    Schema::create('production_information', function (Blueprint $table) {
+		    $table->id();
+		    $table->unsignedBigInteger('information_id');
+		    $table->unsignedBigInteger('product_id');
+		    $table->string('value', 255);
+
+		    $table->timestamps();
+		    $table->softDeletes();
+
+	    });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('p_information');
+    }
+}
