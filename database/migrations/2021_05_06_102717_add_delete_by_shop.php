9e1a8506ea14503d8160fdcc8b3d326fbef16810
quang-lx <quang268200@gmail.com>
2021-05-06 10:29:46 +0700

    add migrations

---
+++ database/migrations/2021_05_06_102717_add_delete_by_shop.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddDeleteByShop extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('shop', function (Blueprint $table) {
+            $table->bigInteger('deleted_by')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('shop', function (Blueprint $table) {
+            //
+        });
+    }
+}
