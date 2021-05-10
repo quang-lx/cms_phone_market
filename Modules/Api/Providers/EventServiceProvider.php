83d537ee37351385eb9d4c2a2a7b5fd71d4643b7
Linh Pham <phamlinh0208@gmail.com>
2021-05-10 15:47:13 +0700

    fx

--- Modules/Api/Providers/EventServiceProvider.php
+++ Modules/Api/Providers/EventServiceProvider.php
@@ -3,24 +3,12 @@
 namespace Modules\Api\Providers;
 
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Modules\Api\Events\FinishLuckyDraw;
-use Modules\Api\Events\MissionDone;
 use Modules\Api\Events\ProductRatingCreated;
 use Modules\Api\Events\ShopRatingCreated;
-use Modules\Api\Events\UserBoughtCard;
-use Modules\Api\Events\UserEarnCoin;
-use Modules\Api\Events\UserRegisted;
-use Modules\Api\Listeners\CheckMissionCompleted;
-use Modules\Api\Listeners\GenerateLuckyDrawHistory;
-use Modules\Api\Listeners\GenerateUserTurnDaily;
-use Modules\Api\Listeners\IncrementUserTurn;
-use Modules\Api\Listeners\InsertCardHistory;
+use Modules\Api\Events\UserSearched;
 use Modules\Api\Listeners\InsertUserSearch;
 use Modules\Api\Listeners\SummaryProductRating;
 use Modules\Api\Listeners\SummaryShopRating;
-use Modules\Api\Listeners\UpdateGamePointUsed;
-use Modules\Api\Listeners\UpdateUserCoin;
-use Modules\Mon\Entities\UserSearch;
 
 class EventServiceProvider extends ServiceProvider {
 	protected $listen = [
@@ -32,7 +20,7 @@ class EventServiceProvider extends ServiceProvider {
 			SummaryShopRating::class
 		],
 
-        UserSearch::class => [
+		UserSearched::class => [
             InsertUserSearch::class
         ]
 
