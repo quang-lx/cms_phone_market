6bf4c303c4c1b21c9a9cf5d054840ea7895972be
quang-lx <quang268200@gmail.com>
2021-05-12 11:17:56 +0700

    add model

---
+++ Modules/Mon/Entities/OrderProduct.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Modules\Mon\Entities;
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Model;
+
+class OrderProduct extends Model
+{
+    use  SoftDeletes;
+
+    protected $table = 'order_product';
+    protected $fillable = [
+        'order_id',
+        'product_id',
+        'product_attribute_value_id',
+        'quatity',
+        'price',
+        'note',
+    ];
+}
