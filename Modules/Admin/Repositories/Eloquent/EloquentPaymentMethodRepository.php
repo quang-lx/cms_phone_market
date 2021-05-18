736c57c1729a91a28f27bff12faa6edf7902cffb
Linh Pham <phamlinh0208@gmail.com>
2021-05-18 15:01:29 +0700

    api payment method

--- Modules/Admin/Repositories/Eloquent/EloquentPaymentMethodRepository.php
+++ Modules/Admin/Repositories/Eloquent/EloquentPaymentMethodRepository.php
@@ -2,12 +2,27 @@
 
 namespace Modules\Admin\Repositories\Eloquent;
 
+use App\Models\CacheKey;
+use Illuminate\Support\Facades\Cache;
 use Modules\Admin\Repositories\PaymentMethodRepository;
 use \Modules\Mon\Repositories\Eloquent\BaseRepository;
 use Illuminate\Http\Request;
 
 class EloquentPaymentMethodRepository extends BaseRepository implements PaymentMethodRepository
 {
+	public function create($data)
+	{
+		$model = $this->model->create($data);
+		Cache::forget(CacheKey::PAYMENT_METHOD_ALL);
+		return $model;
+	}
+
+	public function update($model, $data)
+	{
+		$model->update($data);
+		Cache::forget(CacheKey::PAYMENT_METHOD_ALL);
+		return $model;
+	}
     public function serverPagingFor(Request $request, $relations = null)
     {
         $query = $this->newQueryBuilder();
