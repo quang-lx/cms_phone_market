d4650e7ff1de0360ff52aed7c02fa285ade25d87
Linh Pham <phamlinh0208@gmail.com>
2021-05-18 08:23:21 +0700

    update dat hangfx

--- database/migrations/2021_05_18_074027_migrate_table_order_product.php
+++ database/migrations/2021_05_18_074027_migrate_table_order_product.php
@@ -14,8 +14,10 @@ class MigrateTableOrderProduct extends Migration
     public function up()
     {
 	    Schema::table('order_product', function (Blueprint $table) {
-		    $table->double('sale', 12, 2)->default(0)->nullable()->change();
-		    $table->double('price_sale', 12, 2)->default(0)->nullable()->change();
+		    $table->dropColumn('sale');
+		    $table->dropColumn('price_sale');
+		    $table->double('sale', 12, 2)->default(0)->nullable();
+		    $table->double('price_sale', 12, 2)->default(0)->nullable();
 		    $table->text('note')->nullable();
 		    $table->unsignedBigInteger('product_id')->nullable()->change();
 		    $table->string('product_title', 1024)->nullable();
