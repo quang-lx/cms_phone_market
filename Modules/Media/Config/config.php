144f567582d7e6bec5eed8817bc6b08e31b0473a
Linh Pham <phamlinh0208@gmail.com>
2021-05-28 06:05:43 +0700

    fx image/heic

--- Modules/Media/Config/config.php
+++ Modules/Media/Config/config.php
@@ -6,8 +6,8 @@ return [
     | Specify all the allowed file extensions a user can upload on the server
     |--------------------------------------------------------------------------
     */
-    'allowed-types' => '.jpg,.png,.pdf,.jpeg,.doc,.docx,.ppt,.pptx,.rar,.zip,.mp4,.mov',
-    'allowed-mimetypes' => 'image/jpeg,image/png,image/jpg,image/gif,image/svg+xml,image/svg,application/x-rar-compressed,application/zip,application/msword,application/pdf,application/vnd.ms-powerpoint,application/vnd.openxmlformats-officedocument.presentationml.presentation,video/mp4,video/quicktime',
+    'allowed-types' => '.jpg,.png,.pdf,.jpeg,.doc,.docx,.ppt,.pptx,.rar,.zip,.mp4,.mov,.heic',
+    'allowed-mimetypes' => 'image/heic,image/heif,image/jpeg,image/png,image/jpg,image/gif,image/svg+xml,image/svg,application/x-rar-compressed,application/zip,application/msword,application/pdf,application/vnd.ms-powerpoint,application/vnd.openxmlformats-officedocument.presentationml.presentation,video/mp4,video/quicktime',
     /*
     |--------------------------------------------------------------------------
     | Determine the max file size upload rate
