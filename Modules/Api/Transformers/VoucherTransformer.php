32e757d63022fc2748e34c163b766ec9493e6960
Linh Pham <phamlinh0208@gmail.com>
2021-05-28 06:13:17 +0700

    fx

--- Modules/Api/Transformers/VoucherTransformer.php
+++ Modules/Api/Transformers/VoucherTransformer.php
@@ -4,6 +4,8 @@
 namespace Modules\Api\Transformers;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\DB;
+use Modules\Mon\Entities\Voucher;
 
 class VoucherTransformer extends JsonResource
 {
@@ -11,6 +13,10 @@ class VoucherTransformer extends JsonResource
 
     public function toArray($request)
     {
+        $listProduct = null;
+        if ($this->type == Voucher::TYPE_DISCOUNT_PRODUCT) {
+            $listProduct = $this->listProductId($this->id);
+        }
 
         $data = [
             'id' => $this->id,
@@ -21,11 +27,15 @@ class VoucherTransformer extends JsonResource
             'discount_type' => $this->discount_type,
             'discount_amount' => $this->discount_amount,
             'require_min_amount' => $this->require_min_amount,
+            'products' => $listProduct
 
         ];
 
 
         return $data;
+    }
+    public function listProductId($voucherId) {
+        DB::table('voucher_product')->where('voucher_id', $voucherId)->select('product_id')->pluck('product_id')->all();
     }
 
 }
