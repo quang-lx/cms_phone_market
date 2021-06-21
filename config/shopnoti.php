897304ca90151da66dfff200f16e9f8e7fa970ab
Linh Pham <phamlinh0208@gmail.com>
2021-06-21 14:09:58 +0700

    fx

--- config/shopnoti.php
+++ config/shopnoti.php
@@ -4,11 +4,11 @@ return [
 		'sua_chua'=>[
 			'created' =>  [
 				'title' => 'Khách hàng đặt đơn sửa chữa thành công',
-				'content' => 'Có đơn sửa chữa mới %s, vui lòng xác nhận đơn hàng',
+				'content' => 'Có đơn sửa chữa mới %s, vui lòng xác nhận đơn hàng.',
 			],
 			'wait_client' =>  [
 				'title' => 'Khách hàng xác nhận giá tiền và thời gian',
-				'content' => 'Đơn sửa chữa %s đã được xác nhận, Vui lòng xác nhận sửa chữa',
+				'content' => 'Đơn sửa chữa %s đã được xác nhận, Vui lòng xác nhận sửa chữa.',
 			],
 
 			'done' => [
@@ -23,7 +23,7 @@ return [
 		'bao_hanh'=>[
 			'created' =>  [
 				'title' => 'Khách hàng đặt đơn bảo hành thành công',
-				'content' => 'Có đơn bảo hành mới %s đặt, vui lòng xác nhận đơn hàng',
+				'content' => 'Có đơn bảo hành mới %s đặt, vui lòng xác nhận đơn hàng.',
 			],
 
 			'done' => [
@@ -38,7 +38,7 @@ return [
 		'mua_hang' => [
 			'created' => [
 				'title' => 'Khách hàng đặt đơn thành công',
-				'content' => 'Có đơn hàng mới %s đặt, vui lòng xác nhận đơn hàng',
+				'content' => 'Có đơn hàng mới %s đặt, vui lòng xác nhận đơn hàng.',
 			],
 
 			'done' => [
